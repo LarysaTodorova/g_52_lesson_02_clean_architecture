ed60af6de38f1cbfc3b163ecc4d5116f21bd3af5
Lora <lorageo33@gmail.com>
2026-03-24 18:10:37 +0200

    initial commit

--- src/main/java/app/repository/CarRepositoryHibernate.java
+++ src/main/java/app/repository/CarRepositoryHibernate.java
@@ -9,9 +9,18 @@ import java.util.List;
 
 public class CarRepositoryHibernate implements CarRepository {
 
+
+    // Менеджер сущностей - это объект, через который происходит взаимодействие с БД
     private final EntityManager entityManager;
 
     public CarRepositoryHibernate() {
+        /*
+        Чтобы сконфигурировать энтити менеджер, создаём специальный объект
+        для конфигурации и при помощи метода configure указываем путь к файлу конфигурации.
+        Путь указывается относительно папки resources.
+        После этого метода при помощи метода buildSessionFactory создаём специальный
+        объект "фабрики сессий", при помощи которого и создаём объект энтити менеджера
+         */
         entityManager = new Configuration()
                 .configure("mysql.cfg.xml")
                 .buildSessionFactory()
@@ -20,14 +29,21 @@ public class CarRepositoryHibernate implements CarRepository {
 
     @Override
     public Car save(Car car) {
+        // Получаем объект транзакции у энтити менеджера
         EntityTransaction transaction = entityManager.getTransaction();
         try {
+            // Открываем транзакцию
             transaction.begin();
+            // Сохраняем автомобиль в БД
             entityManager.persist(car);
+            // Закрываем транзакцию
             transaction.commit();
+            // Возвращаем автомобиль (ему уже будет присвоен ИД)
             return car;
         } catch (Exception e) {
+            // В случае ошибки проверяем, активна ли транзакция
             if (transaction.isActive()) {
+                // и если да, то выполняем откат транзакции
                 transaction.rollback();
             }
             throw new RuntimeException(e);
